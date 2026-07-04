232f238abb718b37aa87d80d3dd016c4360842c9
SUMIT MADDESHIYA <sumitmaddeshiya99@gmail.com>
2026-07-04 18:51:42 +0530

    added list-users.sh

--- Backend/src/controllers/Product.controller.js
+++ Backend/src/controllers/Product.controller.js
@@ -1,5 +1,7 @@
 const ProductUrlSchema = require("../models/productUrl");
 const User = require("../models/User");
+const axios = require("axios");
+const cheerio=require('cheerio');
 
 async function submitUrlController(req, res) {
     try {
@@ -28,13 +30,13 @@ async function submitUrlController(req, res) {
            message: "Only Amazon and Flipkart URLs are supported.",
            });
         }
+        console.log("product data::", productUrl);
+        await currentPriceTrackController(productUrl);
 
         await ProductUrlSchema.create({
             userId: user._id,
-            productUrl: productUrl
+            productUrl: productUrl,
         })
-
-        await priceTrackController(productUrl);
 
         return res.status(200).json({
             success: true,
@@ -47,6 +49,56 @@ async function submitUrlController(req, res) {
         })
     }
 } 
+
+const currentPriceTrackController = async (productUrl) => { 
+    try {
+        const {data} = await axios.get(
+      "https://www.flipkart.com/apple-iphone-15-black-128-gb/p/itm6ac6485515ae4",
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+        },
+      }
+    );
+
+    // console.log("Status:", response.status);
+    // console.log(response.data);
+    // console.log(data.substring(0, 1000));
+    const $ = cheerio.load(data);
+
+    console.log("TITLE:", $("title").text());
+
+    console.log("PRODUCT TITLE:", $("span.B_NuCI").text());
+
+    console.log("PRODUCT TITLE 2:", $("span.VU-ZEz").text());
+
+    console.log("PRICE:", $("div.Nx9bqj").first().text());
+//     const $ = cheerio.load(data);
+
+// console.log("H1 Tags:");
+// $("h1").each((i, el) => {
+//   console.log($(el).text().trim());
+// });
+
+// console.log("-----------");
+
+// console.log("Price Candidates:");
+// $("*").each((i, el) => {
+//   const text = $(el).text().trim();
+
+//   if (/₹\s?[\d,]+/.test(text)) {
+//     console.log(
+//       $(el).prop("tagName"),
+//       $(el).attr("class"),
+//       text
+//     );
+//   }
+// });
+    } catch (error) {
+        console.log("Error in pricecontroller.")
+    }
+};
 
 const getAllMyLinksController = async (req, res) => {
     try {
@@ -74,16 +126,8 @@ const getAllMyLinksController = async (req, res) => {
     }
 }
 
-const priceTrackController = async (productUrl) => {
-    try {
-        console.log("recievedUrl::", productUrl);
-    } catch (error) {
-        console.log("Error in pricetrackcontroller::", error);
-    }
-}
-
 module.exports = {
     submitUrlController,
     getAllMyLinksController,
-    priceTrackController
+    // currentPriceTrackController
 }
---
+++ list-user.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+############################
+# About: To list users that has access to a github repo
+#Input: Username 
+#        token
+#Owner:
+############################
+
+#github api url
+API_URL:"https://api.github.com"
+
+#Github username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+#User and Repository information
+REPO_OWNER=$1 #organisation name
+REPO_NAME=$2 # repository name
+
+#Function to make a GET request to GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    #send a GET request to the github api with authentication
+    curl -s -u "&{USERNAME}:${TOKEN}" "$url"
+}
+
+#functionto list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    #fetch the list of collaborators on the repository
+    # collaborators="$(github_api_get "$endpoint" "  --> this will give original json response
+    # collaborators="$(github_api_get "$endpoint" "
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permission.pull == true) | .login')"
+
+    #display the list of collaborators with read acccess
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPPO_OWNER}/${REPO_NAME}:"
+    else
+        echo "Users with read access to ${REPPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi       
+}
+
+#Main script
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
