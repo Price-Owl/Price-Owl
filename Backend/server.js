540d0e7f012be4f89ff073defea2e148087c68dd
SUMIT MADDESHIYA <sumitmaddeshiya99@gmail.com>
2026-07-18 14:42:23 +0530

    price traxking error fix

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 const startCron = require("./src/cron/cron");
 
+const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 dotenv.config();
 const app = require("./src/app");
@@ -10,6 +11,6 @@ const dbConnect = require("./src/config/database");
 dbConnect();
 startCron();
 
-app.listen(5000, (req, res) => {
+app.listen(PORT, (req, res) => {
     console.log("App is listening on port 5000.")
 })
